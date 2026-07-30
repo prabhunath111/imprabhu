eb990742ea2e1f613941b0f8b4f489e86f367b42
prabhu.tiwary <prabhu.tiwary@simplifyhire.com>
2026-07-30 22:17:45 +0530

    Update hero section greeting emoji

--- lib/sections/hero_section.dart
+++ lib/sections/hero_section.dart
@@ -113,7 +113,7 @@ class _HeroText extends StatelessWidget {
       children: [
         RevealOnScroll(
           child: Text(
-            "Hi, I'm 👋",
+            "Hi, I'm 👋 👋",
             style: GoogleFonts.spaceGrotesk(
               color: AppColors.textPrimary,
               fontSize: mobile ? 24 : 28,
